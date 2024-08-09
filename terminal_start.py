7396203ca7c79194a8965dea18f44fde9e30152c
Akshat-M-co <akshatmittal0910@gmail.com>
2024-08-09 10:05:44 +0530

    Update terminal_start.py

--- terminal_start.py
+++ terminal_start.py
@@ -1,7 +1,7 @@
 # This code is to be used in the terminal and takes the input grade level and returns the window.
-print("Welcome to the Adaptive Test Bot")
+print("Welcome to the Adaptive Test Bot.")
+print("this bot will take your doubts and give questions with crystal clear explanations that can help alleviate and solve doubts in your mind.")
 def main():
-    print("Welcome to the Adaptive Test Bot")
     level = input("Please enter your level(grade)\n")
     subj = input("Please enter your subject\n")
     print("Please confirm that you are in grade " + level + " and your subject is " + subj + "\n You may change either by entering 'change'")
