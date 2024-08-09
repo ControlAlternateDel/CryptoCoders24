8b90bfac53575d523bf5b3f60c3e70c60f73ccaf
ControlAltern <soham@chugul.com>
2024-08-09 09:40:40 +0530

    added basic widgets

--- main.py
+++ main.py
@@ -1,2 +1,14 @@
 import os
-import customtkinter
+import customtkinter as ctk
+import pyttsx3
+
+root = ctk.CTk()
+root.geometry("500x300")
+root.resizable(False, False)
+root.title("Adaptive Test Bot (ATB)")
+entry = ctk.CTkEntry(root, width=350)
+submit = ctk.CTkButton(root, width=150, text="submit")
+
+submit.place(x=350, y=270)
+entry.place(x=0, y=270)
+root.mainloop()
