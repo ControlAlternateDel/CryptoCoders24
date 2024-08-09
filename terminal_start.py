20201a1623b8ac69ef1b2ed4f797dd03d744ef95
Akshat-M-co <akshatmittal0910@gmail.com>
2024-08-09 09:36:39 +0530

    Create terminal_start.py

---
+++ terminal_start.py
@@ -0,0 +1,12 @@
+def main():
+  print("Welcome to the adaptive test bot")
+  level = input("Please enter your level(grade)\n")
+  subj = input("Please enter your subject\n")
+  print("Please confirm that you are in grade " + level + " and your subject is " + subj + "\n You may change either by entering 'change'")
+  confirm = input("Please enter 'confirm' to confirm or 'change' to change\n")
+  if confirm == "confirm":
+      print("Confirmed. Please go to the new window.")
+  elif confirm == "change":
+      print("Please re-enter your level and subject.")
+      level = input("Enter your new level")
+      main()
