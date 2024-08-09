836b74de4c8633d7b152c8a4d6e01fb2a6211570
Akshat-M-co <akshatmittal0910@gmail.com>
2024-08-09 09:56:45 +0530

    Update terminal_start.py

--- terminal_start.py
+++ terminal_start.py
@@ -1,13 +1,15 @@
+#This code is to be used in the terminal and takes the input grade level and returns the window.
+print("Welcome to the adaptive test bot")
 def main():
-    print("Welcome to the Adaptive Test bot")
-    level = input("Please enter your level(grade)\n")
-    subj = input("Please enter your subject\n")
-    print("Please confirm that you are in grade " + level + " and your subject is " + subj + "\n You may change either by entering 'change'")
-    confirm = input("Please enter 'confirm' to confirm or 'change' to change\n")
-    if confirm == "confirm":
-        print("Confirmed. Please go to the new window.")
-    elif confirm == "change":
-        print("Please re-enter your level and subject.")
-        level = input("Enter your new level")
-        main()
+  print("Welcome to the adaptive test bot")
+  level = input("Please enter your level(grade)\n")
+  subj = input("Please enter your subject (Please note that you cannot enter a typo subject)\n")
+  print("Please confirm that you are in grade " + level + " and your subject is " + subj + "\n You may change either by entering 'change'")
+  confirm = input("Please enter 'confirm' to confirm or 'change' to change\n")
+  if confirm.lower() == "confirm":
+      print("Confirmed. Please go to the new window.")
+  elif confirm.lower() == "change":
+      print("Please re-enter your level and subject.")
+      main()
+main()
 
