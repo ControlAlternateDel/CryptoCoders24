48cb722650f42e6cf715db5596dd00b42766aecd
ControlAltern <soham@chugul.com>
2024-08-09 10:08:33 +0530

    label and first message

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import os
 import customtkinter as ctk
 import pyttsx3
+from time import sleep
 
 root = ctk.CTk()
 root.geometry("500x300")
@@ -8,6 +9,9 @@ root.resizable(False, False)
 root.title("Adaptive Test Bot (ATB)")
 entry = ctk.CTkEntry(root, width=400)
 submit = ctk.CTkButton(root, width=100, text="Submit")
+label = ctk.CTkLabel(root)
+label.configure(text="Welcome to the Adaptive Test Bot.")
+sleep(2)
 
 submit.place(x=350, y=270)
 entry.place(x=0, y=270)
