9d516142d46a2520ad71d623c8961284c5ed2979
ControlAltern <soham@chugul.com>
2024-08-10 09:37:08 +0530

    everything everywhere all at one

--- main.py
+++ main.py
@@ -1,52 +1,56 @@
 import os
 import customtkinter as ctk
-import subprocess
-import pyttsx3
 import sys
+import subprocess
 
+score = 0
+sys.stdout = open("output.txt", "w")
 root = ctk.CTk()
 root.geometry("500x300")
-root.resizable(False, False)
 root.title("Adaptive Test Bot (ATB)")
 buttonpress = False
 entry = ctk.CTkEntry(root, width=400)
-label = ctk.CTkLabel(root, text=" ")
-
+label = ctk.CTkLabel(root, text=" ", wraplength=300)
 def is_ready(cond):
-    if cond:
-        return True
-    else:
-        return False
+    return(cond)
 def main():
     global value
     global label
     global button
     global root
     global start
-    label.configure(text="Welcome to the Adaptive Test Bot.\nthis bot will take your doubts and give questions with\n crystal clear explanations that can help alleviate\n and solve doubts in your mind.\nPlease enter your level(grade), and your subject, separated by a space\n", require_redraw=True)
+    start.destroy()
+    label.configure(text="Welcome to the Adaptive Test Bot.\nthis bot will take your doubts and give questions with\n crystal clear explanations that can help alleviate\n and solve doubts in your mind.\nPlease enter your level(grade), and your subject/topic, separated by a space\n", require_redraw=True)
 
 def main2():
     global start
     global label
-    start.destroy()
+    global question
     level = entry.get()
+    global grade
     grade, subject = level.split(" ")
-    question = subprocess.check_output(f"ollama run llama3.1 'give me an extremely hard multiple choice question for grade {grade} about {subject}, but dont provide the answer'", shell=True, text=True)
+    question = subprocess.check_output(f"ollama run llama3.1 'give me an extremely hard multiple choice question for grade {grade} about {subject}, but dont provide the answer'", text=True, shell=True)
     label.configure(text=f"{question}")
 
 def get_entry():
-    global button
-    global value
     global entry
+    global grade
+    global label
     value = entry.get()
-    button = True
+    if value.lower() == "quit":
+        root.destroy()
+        quit()
+    elif value.lower() == "answer":
+        answer = subprocess.check_output(f"ollama run llama3.1 'give the answer for the question {question.replace("\n", " ")}, with an in-depth explanation suiting grade {grade} level.' ", text=True, shell=True)
+        label.configure(text=answer)
     return value
 
+command = ctk.CTkButton(root, width=100, text="Command", command=get_entry)
 submit = ctk.CTkButton(root, width=100, text="Submit", command=main2)
 start = ctk.CTkButton(root, text="start", command=main)
-
 submit.place(x=400, y=270)
+command.place(x=400, y=240)
 entry.place(x=0, y=270)
-start.place(x=250, y=220)
+start.place(x=190, y=150)
 label.pack()
 root.mainloop()
